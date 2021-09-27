718f57d15b5a3a89bed141810ab182ed36c41689
Dominik Ruzicka <dominikruzicka323@gmail.com>
2021-09-28 00:08:14 +0200

    create base test structure to deploy

---
+++ src/main/java/com/dominik/restservices/controllers/GreetingController.java
@@ -0,0 +1,21 @@
+package com.dominik.restservices.controllers;
+
+import com.dominik.restservices.models.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    // /greeting?name=Dan //za otaznikem je RequestParam
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
